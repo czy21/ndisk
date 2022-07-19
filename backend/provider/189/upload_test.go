f2280e0c64bb62b842ed645a6e0823cdde0caeee
Bruce CHEN <805899926@qq.com>
2022-07-19 19:05:25 +0800

    #

--- backend/provider/189/upload_test.go
+++ backend/provider/189/upload_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestMd5Slice(t *testing.T) {
 	const sliceItemSize uint64 = 10485760
-	var sliceTotal = math.Ceil(float64(1024*1024*30) / float64(sliceItemSize))
+	var sliceTotal = math.Ceil(float64(1024*1024*10) / float64(sliceItemSize))
 	a := math.Max(1, sliceTotal)
 	fmt.Println(a)
 }
