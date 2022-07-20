c7ee3e6567c698580cd14b8357e52a8f79c3e279
Bruce CHEN <805899926@qq.com>
2022-07-20 11:53:49 +0800

    #

--- backend/controller/boot.go
+++ backend/controller/boot.go
@@ -16,8 +16,7 @@ func Boot() {
 		gin.SetMode(gin.DebugMode)
 	}
 	address := fmt.Sprintf(":%s", viper.GetString("server.port"))
-	log.Infof("TZ: %s Date: %s", time.Local, time.Now())
-	log.Infof("Listening and serving HTTP on %s", address)
+	log.Infof("Listening and serving HTTP on %s Time: %s", address, time.Now())
 	err := http.ListenAndServe(address, ApiEngine())
 	if err != nil && err != http.ErrServerClosed {
 		log.Error(err)
