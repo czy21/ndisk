522915b9f58612b7bf8e994a9238edd63992f0df
Bruce CHEN <805899926@qq.com>
2022-07-29 13:29:30 +0800

    # add webdav basic auth

--- backend/webdav/api.go
+++ backend/webdav/api.go
@@ -7,6 +7,7 @@ import (
 	"github.com/czy21/ndisk/repository"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"golang.org/x/net/webdav"
 	"net/http"
 	"strings"
@@ -45,10 +46,31 @@ func Controller(r *gin.Engine) {
 	}
 	r1 := r.Group(davPrefix)
 	{
+		r1.Use(BasicAuth)
 		r1.Any("/*path", serveFn)
 		r1.Any("", serveFn)
 		for _, t := range constant.WebDavMethods {
 			r1.Handle(t, "/*path", serveFn)
 		}
 	}
+}
+
+func BasicAuth(c *gin.Context) {
+	if c.Request.Method == "OPTIONS" {
+		c.Next()
+		return
+	}
+	username, password, ok := c.Request.BasicAuth()
+	if !ok {
+		c.Writer.Header()["WWW-Authenticate"] = []string{`Basic realm="ndisk"`}
+		c.Status(http.StatusUnauthorized)
+		c.Abort()
+		return
+	}
+	if viper.GetString("dav.username") == username && viper.GetString("dav.password") == password {
+		c.Next()
+		return
+	}
+	c.Status(http.StatusUnauthorized)
+	c.Abort()
 }
