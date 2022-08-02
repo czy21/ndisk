e7de061e4e8e5bde162c8e397b70f5f897cf43eb
Bruce CHEN <805899926@qq.com>
2022-08-02 19:06:56 +0800

    #

--- backend/provider/boot.go
+++ backend/provider/boot.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	_189 "github.com/czy21/ndisk/provider/189"
+	"github.com/czy21/ndisk/provider/baidu"
 )
 
 var providers map[string]FileSystem
@@ -9,6 +10,7 @@ var providers map[string]FileSystem
 func Boot() {
 	providers = make(map[string]FileSystem)
 	providers[string(Cloud189)] = _189.FileSystem{}
+	providers[string(CloudBaiDu)] = baidu.FileSystem{}
 }
 
 func GetProviders() map[string]FileSystem {
--- backend/provider/const.go
+++ backend/provider/const.go
@@ -4,5 +4,5 @@ type CloudKind string
 
 const (
 	Cloud189   CloudKind = "189"
-	CloudLocal CloudKind = "local"
+	CloudBaiDu CloudKind = "baidu"
 )
