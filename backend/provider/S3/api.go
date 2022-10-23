135b6374b8c08f37c4e319a756242f1780630dc5
Bruce CHEN <805899926@qq.com>
2022-10-23 21:33:40 +0800

    #

--- backend/provider/S3/api.go
+++ backend/provider/S3/api.go
@@ -1,1 +1,6 @@
 package S3
+
+type API struct {
+}
+
+
--- backend/provider/S3/fs.go
+++ backend/provider/S3/fs.go
@@ -3,6 +3,7 @@ package S3
 import (
 	"context"
 	"github.com/czy21/ndisk/model"
+	"github.com/minio/minio-go/v6"
 	"golang.org/x/net/webdav"
 	"os"
 	"path/filepath"
@@ -33,6 +34,9 @@ func (fs FileSystem) Rename(ctx context.Context, file model.ProviderFile) error 
 
 func (fs FileSystem) Stat(ctx context.Context, file model.ProviderFile) (os.FileInfo, error) {
 	//TODO implement me
+	account := file.ProviderFolder.Account
+	client, _ := minio.New(account.Endpoint, account.UserName, account.Password, false)
+	print(client)
 	return nil, filepath.SkipDir
 }
 
